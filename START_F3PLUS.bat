4d4ae0bc4e89eb3b33f70ed283ae6aa22c042197
LucidOcelot <62588969+LucidOcelot@users.noreply.github.com>
2026-08-08 21:00:14 -0500

    Update Windows launcher branding for 2.0

--- START_F3PLUS.bat
+++ START_F3PLUS.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableExtensions DisableDelayedExpansion
 cd /d "%~dp0"
-title F3+ 1.16.2 - LucidOcelot
+title F3+ 2.0.0 - LucidOcelot
 
 if not exist "%~dp0WINDOWS_BOOTSTRAP.ps1" (
   echo ERROR: WINDOWS_BOOTSTRAP.ps1 is missing.
