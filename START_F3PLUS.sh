91843f6d1abbd65f171042f90189fc9b969744f5
LucidOcelot <62588969+LucidOcelot@users.noreply.github.com>
2026-08-10 17:39:40 -0500

    Update Linux launcher version

--- START_F3PLUS.sh
+++ START_F3PLUS.sh
@@ -8,7 +8,7 @@ UV_DIR="$RUNTIME/uv"
 UV="$UV_DIR/uv"
 UV_VERSION='0.12.0'
 
-echo 'F3+ 2.5.3 - LucidOcelot'
+echo 'F3+ 2.5.4 - LucidOcelot'
 echo '=================================='
 echo
 echo 'Checking installation...'
